3cada4e2f4e22c9bd277ca2f4001547ad9b60e4a
dangtrungdung1701 <dangtrungdung1701@gmail.com>
2021-07-22 00:01:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero");
+const bookingTour = document.querySelector(".booking-tour");
+const slide = document.querySelector(".slide");
+
+var loading = false;
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+
+  if (scroll + header.offsetHeight > hero.offsetHeight) {
+    header.classList.add("overlay");
+  } else {
+    header.classList.remove("overlay");
+  }
+});
+const show = (data) => {
+  data.forEach((item, index) => {
+    const slide = document.querySelector(".slide");
+    let node = document.createElement("div");
+    node.className = "slide-item";
+    node.innerHTML = `<img src="./images/booking-tour/${item.img}" alt="img${index}">
+    <div class="img-content">
+    <span class="booking">BOOKING</span>
+    <span class="tour">Tour</span>
+    </div>`;
+    slide.append(node);
+  });
+  slideItem();
+};
+
+fetch("http://localhost:3000/images")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => show(data));
+function slideItem() {
+  let slideItems = document.querySelectorAll(".slide-item");
+
+  let index = 2;
+  var mySlide;
+  const slideItemMargin = parseInt(getComputedStyle(slideItems[1]).marginRight);
+  const slideItemWidth = slideItems[index].clientWidth;
+  slideItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+  slideItems[index].classList.add("active");
+  const stopSlider = () => {
+    clearInterval(mySlide);
+  };
+
+  const startSlider = () => {
+    mySlide = setInterval(() => {
+      index++;
+      if (index > slideItems.length - 1) {
+        index = 0;
+      }
+      slide.style.transform = `translateX(${-(
+        slideItemWidth + slideItemMargin
+      )}px)`;
+      slideItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      slideItems[index].classList.add("active");
+      setTimeout(() => {
+        slide.append(slide.firstElementChild);
+        slide.style.transition = "none";
+        slide.style.transform = "translateX(0)";
+        setTimeout(() => {
+          slide.style.transition = "all 0.5s ease-in-out";
+        }, 100);
+      }, 500);
+    }, 3000);
+  };
+
+  const doSlide = () => {
+    startSlider();
+    slideItems.forEach((item) => {
+      item.addEventListener("mouseenter", () => {
+        stopSlider();
+      });
+      item.addEventListener("mouseleave", () => {
+        startSlider();
+      });
+    });
+  };
+  doSlide();
+}
